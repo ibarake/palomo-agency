4ed2e2f0519c1b9d58de53675bd55306a9579098
unknown <issabarake@gmail.com>
2024-03-28 21:06:34 -0500

    creating boiler plate

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import LogoMain from './assets/fullLogoWhite.svg';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="App flex items-center justify-center h-screen w-screen bg-[#DCCCB3]">
+        <header className='flex flex-col items-center justify-center'>
+          <img src={LogoMain} className="App-logo" alt="logo" width={256} />
+          <h1 className="text-3xl font-bold text-gray-500">
+            Palomo Agency
+          </h1>
+        </header>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
-
-export default App
